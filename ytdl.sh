e78a5db3d5be8f9c4ffc7c6558f85c6f82dbc3b9
tj5897 <tj5899@gmail.com>
2021-02-22 18:57:57 -0800

    modified:   ytdl.sh

--- ytdl.sh
+++ ytdl.sh
@@ -59,10 +59,12 @@ function func_singlevideo () {
 	echo "Where would you like to download this video to?"
 	echo "1: On your sdcard under videos!"
 	echo "2: On your phone storage under videos!"
-	echo "3: Custom location"
-	echo "Q: Return to main menu"
-		read n
-		case $n in
+	echo "3: Custom location (NOT DONE YET)"
+	echo "B: Return to main menu"
+	echo "Q: Exits the program"
+		read var_n
+		case $var_n in
+
 			1) echo "Checking if video directory exists"
 			   	if [ -d /storage/sdcard1/videos ]; then
 			   		echo "It exists, continuing!"
@@ -74,6 +76,7 @@ function func_singlevideo () {
 				fi
 			   echo "Beginning download!"
 			   echo "DEBUG: Executing command"
+			   echo "youtube-dl -o /storage/sdcard1/videos/$var_videoname $var_singlevidurl"
 				youtube-dl -o /storage/sdcard1/videos/$var_videoname $var_singlevidurl
 			  echo "Done!" 
 			  echo "Would you like to return to the main menu? (Y/N)"
@@ -81,13 +84,158 @@ function func_singlevideo () {
 				if [[ $var_confirm == y ]] || [[ $var_confirm == Y ]]; then
 					func_mainmenu
 				elif [[ $var_confirm == n ]] || [[ $var_confirm == N ]]; then
-					echo "You picked no, Press enter to quit"
+					echo "You picked no, Press enter to quit program"
 					read -p
-						exit
+						exit 
+				fi ;; 
+
+			  2)  echo "Checking if video directory exists"
+					if [ -d /storage/sdcard0/videos ]; then
+						echo "It exists! continuing!"
+					else
+						echo "Directory missing, creating now"
+							mkdir /storage/sdcard0/videos
+						echo "Directory made!"
+						echo "Proceeding!"
+					fi
+					echo "Starting download!"
+					echo "DEBUG: Executing command"
+					echo "youtube-dl -o /storage/sdcard0/videos/$var_videoname $var_singlevidurl"
+						youtube-dl -o /storage/sdcard0/videos/$var_videoname $var_singlevidurl
+					echo "Done!" 
+					echo "Would you like to return to the main menu? (Y/N)"
+					read var_confirm
+						if [[ $var_confirm == y ]] || [[ $var_confirm == Y ]]; then
+							func_mainmenu
+						elif [[ $var_confirm == n ]] || [[ $var_confirm == N ]]; then
+							echo "You picked no, Press enter to quit program"
+								read -p
+							exit 
+						fi ;; 
+						
+			3) echo "Will add later!"
+					func_singlevideo ;;
+
+				B) echo "Returning to main menu!"
+					func_mainmen;;
+				b) echo "Returning to main menu"
+					func_mainmenu ;;
+					
+				Q) echo "Quitting program"
+					echo "Press enter to close program"
+						read -p
+							exit ;; 
+							
+				q) echo "Quitting program"
+					echo "Press enter to close program"
+						read -p
+							exit ;;
+		esac
+}
+
+function func_singleaudio () {
+	echo "Single audio downloader menu!"
+	echo "Please input the URL of the video you wish to download"
+		read var_singleaudio
+	echo "You have chosen"
+	echo " $var_singleaudio "
+	echo "Is this correct? (Y/N)"
+		read var_confirm
+	 	if [[ $var_confirm == y ]] || [[ $var_confirm == Y ]]; then
+			echo "You chose yes! Proceeding!"
+	 	elif [[ $var_confirm == n ]] || [[ $var_confirm == N ]]; then
+			echo "You picked no. Returning to main menu!"
+			func_mainmenu
+		 fi
+	echo "Please input a name for this video"
+		read var_videoname
+	echo "You have input $var_videoname is this correct? (Y/N)"
+		read var_confirm
+		if [[ $var_confirm == y ]] || [[ $var_confirm == Y ]]; then
+			echo "Proceeding to next step!"
+
+		elif [[ $var_confirm == n ]] || [[ $var_confirm == N ]]; then
+			echo "You have input no"
+			echo "Restarting function"
+			echo "DEBUG: This will be replaced by a check later so you don't have to reinput the video URL"
+			func_singlevideo
+		fi
+	echo "Where would you like to download this audio to?"
+	echo "1: On your sdcard under audio!"
+	echo "2: On your phone storage under audio!"
+	echo "3: Custom location (NOT DONE YET)"
+	echo "B: Return to main menu"
+	echo "Q: Exits the program"
+		read var_n
+		case $var_n in
+
+			1) echo "Checking if audio directory exists"
+			   	if [ -d /storage/sdcard1/audio ]; then
+			   		echo "It exists, continuing!"
+				else 
+					echo "Directory missing, creating now!"
+					mkdir /storage/sdcard1/audio
+					echo "Directory created!"
+					echo "Proceeding!"
 				fi
-		esac 
+			   echo "Beginning download!"
+			   echo "DEBUG: Executing command"
+			   echo "youtube-dl -o /storage/sdcard1/audio/$var_videoname $var_singleaudio"
+				youtube-dl -o /storage/sdcard1/audio/$var_videoname $var_singleaudio
+			  echo "Done!" 
+			  echo "Would you like to return to the main menu? (Y/N)"
+			  	read var_confirm
+				if [[ $var_confirm == y ]] || [[ $var_confirm == Y ]]; then
+					func_mainmenu
+				elif [[ $var_confirm == n ]] || [[ $var_confirm == N ]]; then
+					echo "You picked no, Press enter to quit program"
+					read -p
+						exit 
+				fi ;; 
 
+			  2)  echo "Checking if video directory exists"
+					if [ -d /storage/sdcard0/audio ]; then
+						echo "It exists! continuing!"
+					else
+						echo "Directory missing, creating now"
+							mkdir /storage/sdcard0/audio
+						echo "Directory made!"
+						echo "Proceeding!"
+					fi
+					echo "Starting download!"
+					echo "DEBUG: Executing command"
+					echo "youtube-dl -o /storage/sdcard0/videos/$var_videoname $var_singlevidurl"
+						youtube-dl -o /storage/sdcard0/videos/$var_videoname $var_singleaudio
+					echo "Done!" 
+					echo "Would you like to return to the main menu? (Y/N)"
+					read var_confirm
+						if [[ $var_confirm == y ]] || [[ $var_confirm == Y ]]; then
+							func_mainmenu
+						elif [[ $var_confirm == n ]] || [[ $var_confirm == N ]]; then
+							echo "You picked no, Press enter to quit program"
+								read -p
+							exit 
+						fi ;; 
+						
+			3) echo "Will add later!"
+					func_singlevideo ;;
 
+				B) echo "Returning to main menu!"
+					func_mainmen;;
+				b) echo "Returning to main menu"
+					func_mainmenu ;;
+					
+				Q) echo "Quitting program"
+					echo "Press enter to close program"
+						read -p
+							exit ;; 
+							
+				q) echo "Quitting program"
+					echo "Press enter to close program"
+						read -p
+							exit ;;
+		esac
+		
 }
 
 func_mainmenu
